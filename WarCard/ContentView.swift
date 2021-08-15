6f1c62e247fa4c7fa083172a16dd244b01648419
Andira Yunita <andhirayp@gmail.com>
2021-08-15 10:56:57 +0700

    10-12: state properties using @State & access control private, added buttons with label image and actions, and added some logic to compare 2 cards.

--- WarCard/ContentView.swift
+++ WarCard/ContentView.swift
@@ -3,37 +3,89 @@
 //  WarCard
 //
 //  Created by Andira Yunita on 14/08/21.
-//  Lesson 10. CWC
+//  Lesson 10-12. CWC
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
     var body: some View {
         
-        VStack {
-            
-            //Button instance with closure
-            Button("Click Me", action: {
-                print("Hello World")
-            })
-            
-            
-            //Button instance with trailing closure
-            Button("Click Me") {
-                print("Hello World!")
-            }
-            
-            
-            //Button with label view
-            Button(action: {
-                print("Hella World")
-            }, label: {
-                HStack {
-                    Image(systemName: "pencil") //sf symbol from developer.apple.com/sf-symbols
-                    Text("Edit")
+        ZStack {
+                    
+            Image("background")
+                .ignoresSafeArea()
+                    
+            VStack {
+                    
+                Spacer()
+                Image("logo")
+                Spacer()
+                        
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
                 }
-            })
-            
+                        
+                Spacer()
+                
+                Button(action: {
+                    
+                    let playerRandom = Int.random(in: 2...14)
+                    let cpuRandom = Int.random(in: 2...14)
+                    
+                    //update the cards
+                    playerCard = "card" + String(playerRandom)
+                    cpuCard = "card" + String(cpuRandom)
+                    
+                    //update the score
+                    if playerRandom > cpuRandom {
+                        playerScore += 1
+                    } else if cpuRandom > playerRandom {
+                        cpuScore += 1
+                    }
+                    
+                }, label: {
+                    Image("dealbutton")
+                })
+                
+                Spacer()
+                        
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                            .foregroundColor(Color.white)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                            .foregroundColor(Color.white)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                        }
+                    Spacer()
+                }
+                
+                Spacer()
+                
+            }
         }
         
     }
